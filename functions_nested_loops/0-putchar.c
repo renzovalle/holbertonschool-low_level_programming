a2c75ff32f2764ad75136e3cb92e6876cc1443ed
root <you@example.com>
2023-10-18 23:25:54 -0700

    task 1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,7 +5,6 @@
  * Description: task
  * Return: 0
  */
-
 int main(void)
 {
 	_putchar('_');
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - void
+ *
+ * Description: prints the alphabet
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
